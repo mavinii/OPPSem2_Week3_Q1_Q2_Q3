31e5603737e6b139149d3dbde4407cb215487a23
mavinii <pg.marcosoliveira@icloud.com>
2022-02-10 15:50:12 +0000

    Add project files.

---
+++ Exercises/Exercise1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// 22931 - Marcos Oliveira
+
+//Write a program for a package delivery service. The program contains an array that holds the 
+//ten zip codes to which the company delivers packages. Prompt a user to enter a zip code and 
+//display a message indicating whether the zip code is one to which the company delivers.
+
+namespace OPP_Sem2_Week3_Q1_Q2_Q3.Exercises
+{
+    public class Exercise1
+    {
+        public void deliveryService()
+        {
+            Console.Write("Type your zipe to know info about your delivery: ");
+            int userZipCode = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Test {userZipCode}");
+
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using OPP_Sem2_Week3_Q1_Q2_Q3.Exercises;
+using System;
+
+// 22931 - Marcos Oliveira
+namespace OPP_Sem2_Week3_Q1_Q2_Q3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exercises Week 3\n");
+
+            // Exercise 1
+            Exercise1 exercise1 = new();
+            exercise1.deliveryService();
+        }
+    }
+}
